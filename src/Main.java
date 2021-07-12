a4ef7ab5e9d53cf11702e8b044a6b8b18fc17389
gnevesdev <gnevesdev@protonmail.com>
2021-07-12 17:04:38 +0100

    Basic project with Java window

---
+++ src/Main.java
@@ -0,0 +1,46 @@
+import javax.swing.*;
+import java.awt.*;
+
+class Game extends Canvas implements Runnable
+{
+  static JFrame frame;
+
+  final int CANVAS_WIDTH = 400, CANVAS_HEIGHT = 300;
+  final char CANVAS_SCALE = 2;
+
+  void init_window()
+  {
+    /* THIS FUCKING STUPID IDE DECIDED TO INDENT    */
+    /* THIS SHIT WITH DOUBLE THE SPACES AND I COULD */
+    /* NOT FIX IT, SO FUCK IT! FORGET THIS CRAP,    */
+    /* GLORY TO VIM!                                */
+    this.setPreferredSize(
+            new Dimension(
+                    CANVAS_WIDTH * CANVAS_SCALE,
+                    CANVAS_HEIGHT * CANVAS_SCALE
+            )
+    );
+
+    this.frame = new JFrame("The Tale of Xilda");
+
+    this.frame.add(this);
+    this.frame.setResizable(false);
+    this.frame.pack();
+    this.frame.setLocationRelativeTo(null);
+    this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    this.frame.setVisible(true);
+  }
+
+  @Override
+  public void run()
+  {}
+}
+
+public class Main
+{
+  public static void main(String[] args)
+  {
+    Game game = new Game();
+    game.init_window();
+  }
+}
